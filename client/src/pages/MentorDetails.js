9475ca12bfc23d41618bac61558d9fe1d292edb7
pakkinee.k <pakkinee.k@gmail.com>
2020-04-20 18:27:17 +1000

    DEV-016

--- client/src/pages/MentorDetails.js
+++ client/src/pages/MentorDetails.js
@@ -1,34 +1,40 @@
+/* eslint-disable jsx-a11y/alt-text */
 import React, { useState, useEffect } from 'react'
-import { retrieveMentor } from '../services/retrieve-mentors'
 import { useParams } from 'react-router-dom'
+import { retrieveMentor } from '../services/retrieve-mentors'
 import './Page.css'
 import noPhotoIcon from '../static/no-photo-icon.png'
 
 const MentorDetails = () => {
-    const [mentor, setMentor] = useState([])
-    const { id } = useParams()
+  const [mentor, setMentor] = useState([])
+  const { id } = useParams()
+  useEffect(() => {
+    retrieveMentor(id).then((mentor) => setMentor(mentor))
+  }, [id])
+  const mentorPhoto = mentor.photo
+    ? `https://codesydney-website.s3-ap-southeast-2.amazonaws.com/mentor/${mentor.id}.png`
+    : noPhotoIcon
 
-    useEffect(() => {
-      retrieveMentor(id)
-         .then(mentor => setMentor(mentor))
-    }, [id])
-    
-    const mentorPhoto = mentor.photo ?
-    `https://codesydney-website.s3-ap-southeast-2.amazonaws.com/mentor/${mentor.id}.png` :
-    noPhotoIcon
-
-    return (
-        <div className="mentorpage">
-            <div className="photo">
-                <img className="mentorphoto" src={mentorPhoto} />
-            </div>
-            <div className="mentor-info">
-                <p>Fullname: {mentor.fullName}</p>
-                <p>Title: {mentor.title}</p>
-                <p>Description: {mentor.description}</p>
-
-            </div>
-        </div>
-    )
+  return (
+    <div className="mentorpage">
+      <div className="photo">
+        <img className="mentorphoto" src={mentorPhoto} />
+      </div>
+      <div className="mentor-info">
+        <p>
+          Fullname:
+          {mentor.fullName}
+        </p>
+        <p>
+          Title:
+          {mentor.title}
+        </p>
+        <p>
+          Description:
+          {mentor.description}
+        </p>
+      </div>
+    </div>
+  )
 }
 export default MentorDetails
