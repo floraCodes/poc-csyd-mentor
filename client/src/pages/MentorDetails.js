a5dbe5478b664ef083f6add9755953d6eb31f295
pakkinee.k <pakkinee.k@gmail.com>
2020-04-22 22:56:33 +1000

    DEV-016

--- client/src/pages/MentorDetails.js
+++ client/src/pages/MentorDetails.js
@@ -3,6 +3,7 @@ import React from 'react'
 import { useParams } from 'react-router-dom'
 import { retrieveMentor } from '../services/retrieve-mentors'
 import './Page.css'
+import './Detail.css'
 import noPhotoIcon from '../static/no-photo-icon.png'
 
 const MentorDetails = ({ mentors }) => {
@@ -15,7 +16,6 @@ const MentorDetails = ({ mentors }) => {
   // const mentor  = mentors.filter(mentor => mentor.id === id)
 
   const newMentor = mentors.find(mentor => mentor.id === id)
-  console.log(newMentor)
   const { fullName, description, title, photo } = newMentor
 
   const mentorPhoto = photo
